9c1e3440e6774475d196c8f68e779f1787a4c52d
Murillo <murilloantoniobento@gmail.com>
2025-10-04 15:00:46 -0300

    refactor: substitui getters e setters manuais por Lombok (@Data)

--- src/main/java/com/murilloantoniobento/todolist/user/UserModel.java
+++ src/main/java/com/murilloantoniobento/todolist/user/UserModel.java
@@ -1,35 +1,14 @@
 package com.murilloantoniobento.todolist.user;
 
+import lombok.Data;
 
+@Data
 public class UserModel {
   
   private String username; // Se eu defino public, qualquer classe pode acessar
   private String name; // Se eu não defino nada, é public por padrão
   private String password; // Se eu defino private, só a própria classe pode acessar
 
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setPassword(String password) {
-    this.password = password;
-  }
-  
-  public String getPassword() {
-    return password;
-  }
 
   
 
