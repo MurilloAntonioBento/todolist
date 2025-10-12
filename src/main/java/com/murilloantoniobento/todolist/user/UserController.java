1d3f58d20e39023ebd98c061a3b5d13733705a29
Murillo <murilloantoniobento@gmail.com>
2025-10-12 15:56:09 -0300

    fix: corrige nome da variável passwordHashed no UserController

--- src/main/java/com/murilloantoniobento/todolist/user/UserController.java
+++ src/main/java/com/murilloantoniobento/todolist/user/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
 
     var passwordHashed = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
 
-    userModel.setPassword(passwordHashred);
+    userModel.setPassword(passwordHashed);
 
     var userCreated = this.userRepository.save(userModel);
     return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
