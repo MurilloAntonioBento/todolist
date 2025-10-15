d2901a286eb2c069058a98a63b68b39f2cf8158f
Murillo <murilloantoniobento@gmail.com>
2025-10-15 10:55:41 -0300

    feat: implementar decodificação Basic Auth no FilterTaskAuth

--- src/main/java/com/murilloantoniobento/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/murilloantoniobento/todolist/filter/FilterTaskAuth.java
@@ -1,24 +1,52 @@
 package com.murilloantoniobento.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
   @Override
-  public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-      throws IOException, ServletException {
-    
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+      throws ServletException, IOException {
 
-        chain.doFilter(request, response);
+      // Pegar autenticação (usuario e senha)
+      var authorization = request.getHeader("Authorization");
+
+      var authEncoded = authorization.substring("Basic".length()).trim();
+
+      byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+      var authString = new String(authDecoded);
+
+      
+      String[] credentials = authString.split(":");
+      String username = credentials[0];
+      String password = credentials[1];
+      
+      System.out.println("Authorization");
+      System.out.println(username);
+      System.out.println(password);
+      // Validar usuario
+
+      // Validar senha
+
+      // Segue viagem
+    
+      filterChain.doFilter(request, response);
   }
+
+  
   
 }
