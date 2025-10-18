71224269ce60a4797638955b9d996e7dc5913a95
Murillo <murilloantoniobento@gmail.com>
2025-10-18 14:15:07 -0300

    feat: validar credenciais do usuário no FilterTaskAuth com BCrypt

--- src/main/java/com/murilloantoniobento/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/murilloantoniobento/todolist/filter/FilterTaskAuth.java
@@ -3,9 +3,13 @@ package com.murilloantoniobento.todolist.filter;
 import java.io.IOException;
 import java.util.Base64;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.murilloantoniobento.todolist.user.IUserRepository;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -16,6 +20,9 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class FilterTaskAuth extends OncePerRequestFilter {
+
+  @Autowired
+  private IUserRepository userRepository;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -34,17 +41,26 @@ public class FilterTaskAuth extends OncePerRequestFilter {
       String[] credentials = authString.split(":");
       String username = credentials[0];
       String password = credentials[1];
-      
-      System.out.println("Authorization");
-      System.out.println(username);
-      System.out.println(password);
       // Validar usuario
 
-      // Validar senha
+      var user = this.userRepository.findByUsername(username);
+        if (user == null) {
+          response.sendError(401);
+        } else {
+          // Validar senha
+          var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+          if (passwordVerify.verified) {
+            filterChain.doFilter(request, response);
+          } else {
+            response.sendError(401);
+          }
 
-      // Segue viagem
+          // Segue viagem
     
-      filterChain.doFilter(request, response);
+          
+        }
+
+      
   }
 
   
